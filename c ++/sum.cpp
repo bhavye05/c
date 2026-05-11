b50a3853a88ba290d8ae50736d34300483a26a0c
bhavye05 <bhavye05@gmail.com>
2026-05-12 00:32:15 +0530

    Update sum.cpp

--- c ++/sum.cpp
+++ c ++/sum.cpp
@@ -1,19 +1,29 @@
 #include <iostream>
+#include <cstdlib>   // for atoi()
+
 using namespace std;
 
-int main()
+int main(int argc, char* argv[])
 {
     int n, sum = 0;
 
-    cout << "Enter value of n: ";
-    cin >> n;
+    // Check if command line argument exists
+    if(argc > 1)
+    {
+        n = atoi(argv[1]);
+    }
+    else
+    {
+        cout << "Enter value of n: ";
+        cin >> n;
+    }
 
     for(int i = 1; i <= n; i++)
     {
         sum = sum + i;
     }
 
-    cout << "Sum = " << sum;
+    cout << "Sum = " << sum << endl;
 
     return 0;
 }
