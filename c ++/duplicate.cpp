70695f3e4ff50ba534ab45484401ec6a0ff964a3
bhavye05 <bhavye05@gmail.com>
2026-05-12 01:02:56 +0530

    Update duplicate.cpp

--- c ++/duplicate.cpp
+++ c ++/duplicate.cpp
@@ -10,7 +10,7 @@ int main()
 
     int *a = new int[n];
 
-    cout << "Write your array: ";
+    cout << "Enter array elements: ";
 
     for(int i = 0; i < n; i++)
     {
@@ -21,7 +21,20 @@ int main()
 
     for(int i = 0; i < n; i++)
     {
-        if(a[i] != a[i + 1])
+        int flag = 0;
+
+        // Check previous occurrences
+        for(int j = 0; j < i; j++)
+        {
+            if(a[i] == a[j])
+            {
+                flag = 1;
+                break;
+            }
+        }
+
+        // Print only unique element
+        if(flag == 0)
         {
             cout << a[i] << " ";
         }
